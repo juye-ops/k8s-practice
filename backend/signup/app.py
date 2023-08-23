eaa667998ffd44e91f1cf78c5a42c78ab4e6089f
juye-ops <kjye.ops@gmail.com>
2023-08-23 21:15:46 +0900

    🎨 refactor: set domain for mongodb

--- backend/signup/app.py
+++ backend/signup/app.py
@@ -1,19 +1,24 @@
 import uvicorn
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 from pymongo import MongoClient
 
-mongo_cli = MongoClient("mongodb://localhost:27017/")
+mongo_cli = MongoClient("mongodb://backend-signup-service:27017/")
+db = mongo_cli.k8s
+collection = db.user
 
 app = FastAPI()
 
-class User:
+class User(BaseModel):
     id: str
     password: str
 
 @app.post("/user/add")
 def _add(res: User):
     data = res.dict()
+    collection.insert_one(data)
+    return 200
 
 if __name__ == "__main__":
-    uvicorn.run("app:app", host="0.0.0.0", port=5000, access_log=False)
+    uvicorn.run("app:app", host="0.0.0.0", port=5000)
